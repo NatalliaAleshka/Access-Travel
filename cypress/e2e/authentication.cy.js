c3004707ea7cad7e67ea6da110b269300bbebbdf
NatalliaAleshka <fistashka11578@gmail.com>
2023-04-18 17:47:11 -0400

    modified:   cypress/e2e/authentication.cy.js

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -1,6 +1,6 @@
 ///<reference types ="Cypress"/>
 describe('Authentication', () => {
-    
+
 })
 before(() => {
     cy.visit('https://www.accesstravel.com/en-US/Home/Index')
@@ -12,12 +12,12 @@ it('Should open the Login page', () => {
     cy.url('https://www.accesstravel.com/en-US/Account/Login');
     cy.go('back')
 
-    
+
 })
 
-    it('Should open the Signup page', () => {
-        cy.get('.mobile-menu > :nth-child(3) > :nth-child(2) > a').click({ force: true })
-        cy.url('https://www.accesstravel.com/en-US/Account/Register');
-        cy.go('back')
+it('Should open the Signup page', () => {
+    cy.get('.mobile-menu > :nth-child(3) > :nth-child(2) > a').click({ force: true })
+    cy.url('https://www.accesstravel.com/en-US/Account/Register');
+    cy.go('back')
 
 })
